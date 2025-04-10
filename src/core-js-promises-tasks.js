7aff8a6a44707ef6fcd78338ca3c78f0488d9462
YuliAdam <ylia_adamovich@mail.ru>
2025-04-10 18:27:55 +0200

    feat: add getAllResult function and fix getFirsPromissesResult function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -54,9 +54,7 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises)
-    .then((res) => res)
-    .catch(() => 'Promise rejected');
+  return Promise.any(promises);
 }
 
 /**
@@ -111,8 +109,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((res) =>
+    res.map((r) => (r.status === 'fulfilled' ? r.value : null))
+  );
 }
 
 /**
